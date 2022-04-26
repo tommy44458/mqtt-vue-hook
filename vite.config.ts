27e3c3370d7026eb4646ee11d4b37eea5c1b2d77
Tommy Huang <free44458@gmail.com>
2022-04-26 17:18:27 +0800

    ver. 1.0.3

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), dts()],
     resolve: {
         alias: {
             mqtt: 'mqtt/dist/mqtt.js',
